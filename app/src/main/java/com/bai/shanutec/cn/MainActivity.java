7d12875af6ac5d22f7af6e2cc3c7a725828f505a
zhangi789 <www.392032143@qq.com>
2018-03-22 13:37:11 +0800

    Initial commit

---
+++ app/src/main/java/com/bai/shanutec/cn/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bai.shanutec.cn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/bai/shanutec/cn/MyApi.java
@@ -0,0 +1,19 @@
+package com.bai.shanutec.cn;
+
+import android.app.Application;
+
+/**
+ * @author 张海洋
+ * @Date on 2018/03/09.
+ * @org 上海相舆科技有限公司
+ * @describe
+ */
+
+public class MyApi  extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
