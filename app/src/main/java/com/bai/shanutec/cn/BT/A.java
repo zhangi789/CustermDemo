f41c38835dec9e80027b7ba6571d0a7f8a3a3041
zhangi789 <www.392032143@qq.com>
2018-03-22 13:59:28 +0800

    第三次次提交测试github

---
+++ app/src/main/java/com/bai/shanutec/cn/BT/A.java
@@ -0,0 +1,11 @@
+package com.bai.shanutec.cn.BT;
+
+/**
+ * @author 张海洋
+ * @Date on 2018/03/22.
+ * @org 上海相舆科技有限公司
+ * @describe
+ */
+
+public class A {
+}
---
+++ app/src/main/java/com/bai/shanutec/cn/ui/B.java
@@ -0,0 +1,16 @@
+package com.bai.shanutec.cn.ui;
+
+/**
+ * @author 张海洋
+ * @Date on 2018/03/22.
+ * @org 上海相舆科技有限公司
+ * @describe
+ */
+
+public class B {
+
+
+    public int a(int age) {
+        return 4;
+    }
+}
